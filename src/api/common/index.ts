ca39de690c57e659d2007da4a5569eccac21ed1d
wansongtao <18101837209@163.com>
2023-12-10 10:52:26 +0800

    feat: 添加获取验证码、登录、登出接口

---
+++ src/api/common/index.ts
@@ -0,0 +1,31 @@
+import instance from "@/utils/request";
+import type { IBaseResponse } from "@/types/index";
+import type { ILoginParams } from "@/types/common/index";
+
+export const getCaptcha = () => {
+  return instance.request<any, IBaseResponse<string>>({
+    url: '/admin/captcha',
+    method: 'GET',
+    headers: {
+      isToken: false
+    }
+  })
+}
+
+export const setLogin = (data: ILoginParams) => {
+  return instance.request<any, IBaseResponse<string>>({
+    url: '/admin/login',
+    method: 'POST',
+    headers: {
+      isToken: false
+    },
+    data
+  })
+}
+
+export const setLogout = () => {
+  return instance.request<any, IBaseResponse<null>>({
+    url: '/admin/logout',
+    method: 'POST'
+  })
+}
