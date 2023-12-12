02e5c30baf47aea55abc41647d0a763a55217089
wansongtao <18101837209@163.com>
2023-12-12 22:48:42 +0800

    feat: 在store中保存生成的菜单等

--- src/stores/setting.ts
+++ src/stores/setting.ts
@@ -2,6 +2,9 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { getTheme, setTheme } from '@/utils/theme'
 import { getSystemTheme } from '@/utils/index'
+import { getMenus } from '@/api/common/index'
+import { generateRoutes, generateAsideMenu } from '@/utils/menu'
+import type { IMenuItem } from '@/types/index'
 
 export const useSettingStore = defineStore('setting', () => {
   const theme = ref(getTheme())
@@ -23,11 +26,26 @@ export const useSettingStore = defineStore('setting', () => {
     collapsed.value = !collapsed.value
   }
 
+  const cacheRoutes = ref<string[]>([])
+  const menus = ref<IMenuItem[]>([])
+  async function getRoutesAction() {
+    const result = await getMenus().catch(() => ({ data: [] }))
+
+    const { routes, cacheRouteNames } = generateRoutes(result.data)
+    cacheRoutes.value = cacheRouteNames
+    menus.value = generateAsideMenu(routes)
+
+    return routes
+  }
+
   return {
     theme,
     followSystemTheme,
     toggleTheme,
     collapsed,
-    toggleCollapsed
+    toggleCollapsed,
+    cacheRoutes,
+    menus,
+    getRoutesAction
   }
 })
