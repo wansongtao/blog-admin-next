e346ba38856f4ebbda8da9ddd9cfc505c07d8edb
wansongtao <18101837209@163.com>
2023-12-14 20:49:37 +0800

    refactor: 密码校验函数重构

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -18,9 +18,9 @@ export const validatePassword = async (_rule: Rule, value: string) => {
     return Promise.reject('请输入密码')
   }
 
-  const regexp = /^[a-zA-Z][\w.?!&_]{5,15}$/
+  const regexp = /^[a-zA-Z](?=.*[.?!&_])(?=.*\d)[a-zA-Z\d.?!&_]{5,15}$/
   if (!regexp.test(value)) {
-    return Promise.reject('请输入6至16位由字母、数字、‘.?!&_’特殊字符组成的密码（字母开头）！')
+    return Promise.reject('请输入6至16位由字母、数字、.?!&_特殊字符组成的密码(字母开头，必须包含字母、数字、特殊字符)')
   }
 
   return Promise.resolve()
