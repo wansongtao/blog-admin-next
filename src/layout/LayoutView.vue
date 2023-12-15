b1f1a169e2f1940efdc76abad33f97e37e2f24b7
wansongtao <18101837209@163.com>
2023-12-15 20:38:06 +0800

    refactor: 重构parent view与layout view组件，以支持缓存组件

--- src/layout/LayoutView.vue
+++ src/layout/LayoutView.vue
@@ -2,13 +2,7 @@
 import AsideView from './components/AsideView.vue'
 import HeaderView from './components/HeaderView.vue'
 import ScrollBar from './components/ScrollBar.vue'
-import { useSettingStore } from '@/stores/setting';
-import { computed } from 'vue';
-
-const settingStore = useSettingStore()
-const include = computed(() => {
-  return settingStore.cacheRoutes
-})
+import ParentView from '@/components/ParentView/ParentView.vue';
 </script>
 
 <template>
@@ -18,13 +12,7 @@ const include = computed(() => {
       <HeaderView />
       <ScrollBar />
       <div class="layout-main st-transition-bg">
-        <router-view v-slot="{ Component }">
-          <transition name="slide-fade">
-            <KeepAlive :include="include">
-              <component :is="Component" />
-            </KeepAlive>
-          </transition>
-        </router-view>
+        <ParentView />
       </div>
     </div>
   </div>
@@ -49,63 +37,4 @@ const include = computed(() => {
   }
 }
 
-.slide-fade-enter-active {
-  animation: slide-fade 0.3s both;
-}
-
-.slide-fade-leave-active {
-  display: none;
-}
-
-@keyframes slide-fade {
-  from {
-    transform: translateX(120px);
-    opacity: 0;
-  }
-  to {
-    transform: translateX(0);
-    opacity: 1;
-  }
-}
-
-// .slide-fade-enter-active {
-//   animation: bounce-in-right 0.5s both;
-// }
-
-// @keyframes bounce-in-right {
-//   0% {
-//     transform: translateX(100px);
-//     animation-timing-function: ease-in;
-//     opacity: 0;
-//   }
-//   60% {
-//     transform: translateX(0);
-//     animation-timing-function: ease-out;
-//     opacity: 1;
-//   }
-//   72% {
-//     transform: translateX(48px);
-//     animation-timing-function: ease-in;
-//   }
-//   81% {
-//     transform: translateX(0);
-//     animation-timing-function: ease-out;
-//   }
-//   85% {
-//     transform: translateX(22px);
-//     animation-timing-function: ease-in;
-//   }
-//   90% {
-//     transform: translateX(0);
-//     animation-timing-function: ease-out;
-//   }
-//   95% {
-//     transform: translateX(8px);
-//     animation-timing-function: ease-in;
-//   }
-//   100% {
-//     transform: translateX(0);
-//     animation-timing-function: ease-out;
-//   }
-// }
 </style>
