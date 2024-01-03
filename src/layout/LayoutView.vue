2319710072239a0815d973c7c4debd78067a0b36
wansongtao <18101837209@163.com>
2024-01-03 16:44:45 +0800

    feat: 显式设置主体宽度，处理边界情况

--- src/layout/LayoutView.vue
+++ src/layout/LayoutView.vue
@@ -2,13 +2,22 @@
 import AsideView from './components/AsideView.vue'
 import HeaderView from './components/HeaderView.vue'
 import ScrollBar from './components/ScrollBar.vue'
-import ParentView from '@/components/ParentView/ParentView.vue';
+import ParentView from '@/components/ParentView/ParentView.vue'
+
+import { computed } from 'vue'
+import { useSettingStore } from '@/stores/setting'
+
+// 显式设置width，解决某些特殊情况下，响应式宽度失效的问题（例如：a-table设置了scroll属性）。
+const setStore = useSettingStore()
+const style = computed(() => {
+  return  `width: calc(100% - var(${setStore.collapsed ? '--st-aside-w-s' : '--st-aside-w'}));`
+})
 </script>
 
 <template>
   <div class="layout flex">
     <AsideView />
-    <div class="layout_container">
+    <div class="layout_container" :style="style">
       <HeaderView />
       <ScrollBar />
       <div class="layout-main st-transition-bg">
@@ -36,5 +45,4 @@ import ParentView from '@/components/ParentView/ParentView.vue';
     background-color: var(--st-c-bg-2);
   }
 }
-
 </style>
