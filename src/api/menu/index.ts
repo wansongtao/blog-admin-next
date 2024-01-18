3fd3e2e3688b99c2258a6c32feac23013e6e4186
wansongtao <18101837209@163.com>
2024-01-18 15:47:04 +0800

    feat: 添加获取菜单列表接口

--- src/api/menu/index.ts
+++ src/api/menu/index.ts
@@ -1,9 +1,18 @@
 import instance from '@/utils/request'
-import type { IBaseResponse } from '@/types/index'
+import type { IBaseResponse, IBaseList } from '@/types/index'
+import type { IQueryMenuParams, IMenuTreeItem } from '@/types/menu/index'
 
 export const getMenuTypes = () => {
   return instance.request<any, IBaseResponse<{ id: number; name: string }[]>>({
     url: '/admin/menu/type',
     method: 'GET'
+  })
+}
+
+export const getMenuTree = (params: IQueryMenuParams) => {
+  return instance.request<any, IBaseResponse<IBaseList<IMenuTreeItem>>>({
+    url: '/admin/menu',
+    method: 'GET',
+    params
   })
 }
