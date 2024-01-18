a3704d702bf5053ae5151dc5dbb2b1b0622429ff
wansongtao <18101837209@163.com>
2024-01-18 15:56:32 +0800

    feat: 添加菜单状态配置

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,15 @@
 import type { ITheme } from '@/types/index'
+import type { IQueryMenuParams } from '@/types/menu/index'
 
 export const defaultTheme: ITheme = 'light'
+
+export const MENU_STATUS: { label: string; value: IQueryMenuParams['disabled'] }[] = [
+  {
+    label: '启用',
+    value: 1
+  },
+  {
+    label: '禁用',
+    value: 0
+  }
+]
