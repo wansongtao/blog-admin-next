ac20eba2fac5f068d73cd0d3b3859a1c2f8960f2
wansongtao <18101837209@163.com>
2024-08-27 11:21:29 +0800

    init: 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { zhCN, dateZhCN } from 'naive-ui'
+</script>
+
+<template>
+  <n-config-provider :locale="zhCN" :date-locale="dateZhCN">
+    <router-view />
+  </n-config-provider>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { URL, fileURLToPath } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import svgLoader from 'vite-svg-loader'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'pinia',
+        {
+          'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar']
+        }
+      ]
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    }),
+    svgLoader()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  base: '/admin/',
+  server: {
+    open: true
+  }
+})
