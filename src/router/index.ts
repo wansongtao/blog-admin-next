8b1a572699e1b2d83af12360ca478cf366ad8cfb
wansongtao <18101837209@163.com>
2024-09-03 10:07:40 +0800

    feat: 添加登录、404页面路由及路由meta类型

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '@/views/login/LoginView.vue'
+import NotFound from '@/views/404/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/login',
+      name: 'Login',
+      component: LoginView,
+      meta: {
+        title: '登录',
+        hidden: true
+      }
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFound,
+      meta: {
+        title: '404',
+        hidden: true
+      }
     }
-  ]
+  ],
+  scrollBehavior(to, _from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+
+    if (to.hash) {
+      return {
+        el: to.hash
+      }
+    }
+
+    return {
+      top: 0,
+      behavior: 'smooth'
+    }
+  }
 })
 
 export default router
