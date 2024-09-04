6a470e0f764160f62081b6a41415f1e30902fcaa
wansongtao <18101837209@163.com>
2024-09-04 10:45:21 +0800

    chore: 增加环境配置

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,12 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_BASE_API: string
+  readonly VITE_TITLE: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
 declare const __APP_VERSION__: string
