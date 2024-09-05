0dee9080c51097ea1c849df98447c1cd7a36a326
wansongtao <18101837209@163.com>
2024-09-05 15:54:34 +0800

    feat(layout): 添加侧边菜单栏组件

---
+++ src/layout/components/AsideView.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import getStaticAdminRoute from '@/router/adminRoute'
+import { generateMenus } from '@/utils/menu'
+
+const menus = generateMenus(getStaticAdminRoute().children!)
+const collapsed = ref(false)
+
+const router = useRouter()
+const onJumpPage = (path: string) => {
+  router.push(path)
+}
+</script>
+
+<template>
+  <n-layout-sider
+    bordered
+    :native-scrollbar="false"
+    collapse-mode="width"
+    :collapsed-width="64"
+    :width="240"
+    :collapsed="collapsed"
+  >
+    <n-menu
+      :collapsed="collapsed"
+      :collapsed-width="64"
+      :collapsed-icon-size="22"
+      :options="menus"
+      @update:value="onJumpPage"
+    />
+  </n-layout-sider>
+</template>
+
+<style lang="scss" scoped></style>
