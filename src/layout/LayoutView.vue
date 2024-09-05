e9224bdbf8984963ca812588d3ae4196ef05c006
wansongtao <18101837209@163.com>
2024-09-05 15:55:09 +0800

    feat(layout): 添加layout组件

---
+++ src/layout/LayoutView.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import AsideView from './components/AsideView.vue'
+</script>
+
+<template>
+  <n-layout style="height: 100vh" has-sider>
+    <aside-view />
+    <n-layout>
+      <header>header</header>
+      <main>
+        <parent-view />
+      </main>
+    </n-layout>
+  </n-layout>
+</template>
+
+<style lang="scss" scoped></style>
