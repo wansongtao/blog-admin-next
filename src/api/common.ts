f28ec53341857903de0caa9e3bef8a69533c7a09
wansongtao <18101837209@163.com>
2024-09-05 10:59:56 +0800

    feat(api): 添加登录接口

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,5 +1,7 @@
 import request from '@/utils/axiosRequest'
 
+import type { ILoginParams } from '@/types/api/common'
+
 export const getCaptcha = () => {
   return request<{ captcha: string }>({
     url: '/auth/captcha',
@@ -7,5 +9,16 @@ export const getCaptcha = () => {
     headers: {
       isToken: false
     }
+  })
+}
+
+export const setLogin = (data: ILoginParams) => {
+  return request<{ token: string; refreshToken: string }>({
+    url: '/auth/login',
+    method: 'POST',
+    headers: {
+      isToken: false
+    },
+    data
   })
 }
