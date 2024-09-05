ffc14c3a31b4a9f1f44b66a5367e6a6ce4ab56a8
wansongtao <18101837209@163.com>
2024-09-05 11:31:59 +0800

    feat(userStore): 添加登录方法

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,12 +1,27 @@
+import { setLogin } from '@/api/common'
+
+import type { ILoginParams } from '@/types/api/common'
+
 export const useUserStore = defineStore(
   'user',
   () => {
     const accessToken = ref('')
     const refreshToken = ref('')
+    async function login(data: ILoginParams) {
+      const [, res] = await setLogin(data)
+      if (!res) {
+        return false
+      }
+
+      accessToken.value = res.data.token
+      refreshToken.value = res.data.refreshToken
+      return true
+    }
 
     return {
       accessToken,
-      refreshToken
+      refreshToken,
+      login
     }
   },
   { persist: true }
