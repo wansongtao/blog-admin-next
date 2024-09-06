271445252ff11ca558f8009b09c914723cd7d578
wansongtao <18101837209@163.com>
2024-09-06 15:41:33 +0800

    feat(app): 添加主题切换功能

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script lang="ts" setup>
-import { zhCN, dateZhCN } from 'naive-ui'
+import { zhCN, dateZhCN, darkTheme, lightTheme } from 'naive-ui'
+import { useAppSetStore } from '@/stores/appSet'
+
+const appSetStore = useAppSetStore()
+const { theme } = storeToRefs(appSetStore)
+const themeValue = computed(() => {
+  return theme.value === 'light' ? lightTheme : darkTheme
+})
 </script>
 
 <template>
-  <n-config-provider :locale="zhCN" :date-locale="dateZhCN">
+  <n-config-provider :locale="zhCN" :date-locale="dateZhCN" :theme="themeValue">
     <the-app-provider>
       <router-view />
     </the-app-provider>
