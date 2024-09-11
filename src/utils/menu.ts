003de8cf5ed543188b515042544ccc0390eb17d5
wansongtao <18101837209@163.com>
2024-09-11 16:43:53 +0800

    feat(utils): 添加生成路由方法

--- src/utils/menu.ts
+++ src/utils/menu.ts
@@ -1,5 +1,8 @@
 import MENU_ICON_MAP from '@/plugins/menuIcons'
+import ROUTE_COMPONENT_MAP from '@/plugins/routeComponents'
+import TheParentView from '@/components/the-parent-view/TheParentView.vue'
 
+import type { IMenuItem } from '@/types/api/common'
 import type { MenuOption } from 'naive-ui'
 import type { RouteRecordRaw } from 'vue-router'
 
@@ -64,4 +67,64 @@ export const generateCacheRoutes = (
   })
 
   return cacheRouteNames
+}
+
+export const generateRoutes = (menuTree: IMenuItem[]) => {
+  const historyRouteNames: string[] = []
+
+  const recursionGenerateRoutes = (tree: IMenuItem[], parentPath = '') => {
+    return tree.map((item) => {
+      const path = getFullPath(item.path, parentPath)
+      const name = path.replace(/\//g, '') + '-' + Math.random().toString(36).slice(2)
+
+      const route: RouteRecordRaw = {
+        path,
+        component: TheParentView,
+        name,
+        meta: {}
+      }
+
+      if (item.component && ROUTE_COMPONENT_MAP[item.component]) {
+        route.component = ROUTE_COMPONENT_MAP[item.component]
+
+        const name = item.component
+          .replace(/.vue|.jsx|.tsx/g, '')
+          .split('/')
+          .pop()!
+        if (!historyRouteNames.includes(name)) {
+          route.name = name
+          historyRouteNames.push(name)
+        }
+      }
+
+      if (item.name) {
+        route.meta!.title = item.name
+      }
+      if (item.icon) {
+        route.meta!.icon = item.icon
+      }
+      if (item.cache === true) {
+        route.meta!.cache = item.cache
+      }
+      if (item.hidden === true) {
+        route.meta!.hidden = item.hidden
+      }
+      if (item.props === true) {
+        route.props = item.props
+      }
+
+      if (item.children?.length) {
+        // @ts-ignore
+        route.redirect = item.redirect || getFullPath(item.children[0].path, route.path)
+
+        // @ts-ignore
+        route.children = [...recursionGenerateRoutes(item.children, route.path)]
+      }
+
+      return route
+    })
+  }
+
+  const routes = recursionGenerateRoutes(menuTree)
+  return routes
 }
