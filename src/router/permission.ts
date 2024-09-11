e585b41d43cb470dea8ad8501ddaa58e144a1f39
wansongtao <18101837209@163.com>
2024-09-11 10:00:02 +0800

    feat(router): 退出登录后移除menubar数据

--- src/router/permission.ts
+++ src/router/permission.ts
@@ -2,6 +2,7 @@ import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 import router from './index'
 import { useUserStore } from '@/stores/user'
+import { useAppSetStore } from '@/stores/appSet'
 
 NProgress.configure({ showSpinner: false })
 
@@ -10,9 +11,14 @@ router.beforeEach(async (to) => {
 
   const userStore = useUserStore()
   const token = userStore.accessToken
-  if (!token && to.name !== 'Login') {
-    NProgress.done()
-    return `/login?redirect=${to.path}`
+  if (!token) {
+    const appSetStore = useAppSetStore()
+    appSetStore.menubarItems = []
+
+    if (to.name !== 'Login') {
+      NProgress.done()
+      return `/login?redirect=${to.path}`
+    }
   }
 
   if (token && to.name === 'Login') {
