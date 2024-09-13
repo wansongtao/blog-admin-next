688bd08f19b5def00b9334f242b159d21aa83031
wansongtao <18101837209@163.com>
2024-09-13 14:42:43 +0800

    feat(components): 添加状态选择器组件

---
+++ src/components/select/SelectState.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { STATUS } from '@/constants'
+
+const { placeholder = '状态', clearable = true } = defineProps<{
+  placeholder?: string
+  clearable?: boolean
+}>()
+const disabled = defineModel<(typeof STATUS)[0]['value']>()
+</script>
+
+<template>
+  <n-select v-model:value="disabled" :options="STATUS" :placeholder :clearable />
+</template>
+
+<style lang="scss" scoped></style>
