98a2b76a4e0fcffa25ae288ccce21e95a7aee1ea
wansongtao <18101837209@163.com>
2024-09-23 14:39:25 +0800

    feat(directives): 添加角色/权限校验指令

---
+++ src/directives/vRole.ts
@@ -0,0 +1,19 @@
+import type { App } from 'vue'
+import { useUserStore } from '@/stores/user'
+import { hasRoles } from '@/utils/authority'
+
+const vRole = (app: App) => {
+  app.directive<HTMLElement, string[] | string>('role', (el, binding) => {
+    const userStore = useUserStore()
+    const roles = userStore.userInfo.roles
+    const needRole = binding.value
+    const or = binding.modifiers.or
+
+    const isHas = hasRoles(roles, needRole, or)
+    if (!isHas) {
+      el.parentNode?.removeChild(el)
+    }
+  })
+}
+
+export default vRole
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ import pinia from './stores'
 import App from './App.vue'
 import router from './router'
 import './router/permission'
+import installDirective from './directives'
 
 const app = createApp(App)
+installDirective(app)
 
 app.use(pinia)
 app.use(router)
