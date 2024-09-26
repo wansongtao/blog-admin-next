aa68a5a7be902b566137bdd54703c9f1996914ab
wansongtao <18101837209@163.com>
2024-09-26 16:06:24 +0800

    feat(components): 添加菜单树选择组件

---
+++ src/components/menu-tree-select/MenuTreeSelect.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import useMenuTree from '@/hooks/useMenuTree'
+
+const { containButton = false } = defineProps<{
+  containButton?: boolean
+}>()
+
+const menuId = defineModel<number>()
+const { menuTree } = useMenuTree(containButton)
+</script>
+
+<template>
+  <n-tree-select
+    clearable
+    key-field="id"
+    label-field="name"
+    :options="menuTree as any[]"
+    v-model:value="menuId"
+  />
+</template>
+
+<style lang="scss" scoped></style>
