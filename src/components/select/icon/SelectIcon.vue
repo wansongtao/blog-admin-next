23f0a57cbc3cad79e7c77ea81e34f84d0fcfb64f
wansongtao <18101837209@163.com>
2024-09-26 16:05:37 +0800

    feat(components): 添加选择图标组件

---
+++ src/components/select/icon/SelectIcon.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import MENU_ICON_MAP from '@/plugins/menuIcons'
+import IconArrowDown from '@/assets/svgs/icons/arrow-down.svg?component'
+
+const icon = defineModel<string>()
+const handleCheck = (key: string) => {
+  icon.value = key
+}
+</script>
+
+<template>
+  <n-popover trigger="click">
+    <template #trigger>
+      <n-input v-model:value="icon" placeholder="请选择图标" clearable>
+        <template v-if="icon" #prefix>
+          <component :is="MENU_ICON_MAP[icon]"></component>
+        </template>
+        <template #suffix>
+          <n-icon :component="IconArrowDown"></n-icon>
+        </template>
+      </n-input>
+    </template>
+    <n-row :gutter="[0, 8]">
+      <n-col v-for="(value, key) of MENU_ICON_MAP" :key="key" :span="6">
+        <select-icon-item :name="key" :checked="icon === key" @click="handleCheck(key)">
+          <component :is="value" />
+        </select-icon-item>
+      </n-col>
+    </n-row>
+  </n-popover>
+</template>
+
+<style lang="scss" scoped></style>
