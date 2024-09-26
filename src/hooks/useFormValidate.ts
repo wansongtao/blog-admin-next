c13a0de5a1b95fe0ce95b2944b8cd4f89c4c9a78
wansongtao <18101837209@163.com>
2024-09-26 16:09:56 +0800

    feat(hooks): 表单校验添加可选字段控制

--- src/hooks/useFormValidate.ts
+++ src/hooks/useFormValidate.ts
@@ -3,16 +3,24 @@ import type { FormInst } from 'naive-ui'
 export default function useFormValidate(key = 'formRef') {
   const formRef = useTemplateRef<FormInst>(key)
 
-  const validateForm = () => {
+  const validateForm = <T = string>(validatePartial?: (key: T) => boolean) => {
     return new Promise<boolean>((resolve) => {
-      formRef.value?.validate((valid) => {
-        if (!valid) {
-          resolve(true)
-          return
-        }
+      formRef.value?.validate(
+        (valid) => {
+          if (!valid) {
+            resolve(true)
+            return
+          }
 
-        resolve(false)
-      })
+          resolve(false)
+        },
+        (rule) => {
+          if (validatePartial && rule.key) {
+            return validatePartial(rule.key as T)
+          }
+          return true
+        }
+      )
     })
   }
 
