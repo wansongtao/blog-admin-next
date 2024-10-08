334f8f0428a3d5b68a4d8c4800e959ac26c5e56f
wansongtao <18101837209@163.com>
2024-10-08 16:46:50 +0800

    feat(views): 完成角色页基础功能开发

--- src/views/system/role/RoleView.vue
+++ src/views/system/role/RoleView.vue
@@ -1,7 +1,110 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import SearchForm from './components/SearchForm.vue'
+import useRequest from '@/hooks/useRequest'
+import useTableSort from '@/hooks/useTableSort'
+import { getRoleList } from '@/api/role'
+
+import type { IQuery } from '@/types/api'
+import type { DataTableColumn } from 'naive-ui'
+import type { IRoleListItem } from '@/types/api/role'
+
+const { sort, onSorterChange } = useTableSort()
+
+const { page, pageSize, list, total, loading, fetchList } = useRequest(async (params: IQuery) => {
+  const [, result] = await getRoleList(params)
+  const data = result?.data.list ?? []
+  const total = result?.data.total ?? 0
+
+  return {
+    data,
+    total
+  }
+})
+
+const search = ref<IQuery>({})
+const updateTableData = () => {
+  fetchList({ ...search.value, sort: sort.value })
+}
+
+const onSearch = (data: IQuery) => {
+  search.value = data
+  page.value = 1
+}
+const onReset = () => {
+  search.value = {}
+  page.value = 1
+}
+
+watch(
+  [page, pageSize, search, sort],
+  () => {
+    updateTableData()
+  },
+  { immediate: true }
+)
+
+type IColumn = DataTableColumn<IRoleListItem> & { key?: keyof IRoleListItem | 'action' }
+const columns: IColumn[] = [
+  {
+    align: 'center',
+    key: 'id',
+    title: '角色ID'
+  },
+  {
+    align: 'center',
+    key: 'name',
+    title: '角色名称'
+  },
+  {
+    align: 'center',
+    key: 'description',
+    title: '角色描述'
+  },
+  {
+    align: 'center',
+    key: 'disabled',
+    title: '状态',
+    render: ({ disabled }) => {
+      return disabled ? '禁用' : '启用'
+    }
+  },
+  {
+    align: 'center',
+    key: 'createdAt',
+    title: '创建时间',
+    width: 200
+  },
+  {
+    align: 'center',
+    key: 'updatedAt',
+    title: '更新时间',
+    width: 200
+  }
+]
+</script>
 
 <template>
-  <base-box>role</base-box>
+  <base-box>
+    <search-form :loading @search="onSearch" @reset="onReset" />
+    <div class="table">
+      <n-data-table
+        :columns="columns"
+        :data="list"
+        :loading
+        :cascade="false"
+        style="height: 100%"
+        flex-height
+        striped
+        @update:sorter="onSorterChange"
+      />
+    </div>
+    <base-pagination v-model:page="page" v-model:page-size="pageSize" :total :disabled="loading" />
+  </base-box>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.table {
+  flex: 1;
+  margin: 20px 0;
+}
+</style>
