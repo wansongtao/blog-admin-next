0116822f61e1aac60694e9b6c18994810278b9b1
wansongtao <18101837209@163.com>
2024-10-09 09:59:36 +0800

    refactor(components): 重构菜单树选择组件以支持更多属性

--- src/components/menu-tree-select/MenuTreeSelect.vue
+++ src/components/menu-tree-select/MenuTreeSelect.vue
@@ -1,17 +1,25 @@
-<script lang="ts" setup>
+<script lang="ts" generic="T extends string | number | (string | number)[] | null" setup>
 import useMenuTree from '@/hooks/useMenuTree'
 
 const { containButton = false } = defineProps<{
   containButton?: boolean
+  checkable?: boolean
+  maxTagCount?: number | 'responsive'
+  multiple?: boolean
+  placeholder?: string
 }>()
 
-const menuId = defineModel<number>()
+const menuId = defineModel<T>()
 const { menuTree } = useMenuTree(containButton)
 </script>
 
 <template>
   <n-tree-select
     clearable
+    :placeholder
+    :checkable
+    :multiple
+    :max-tag-count="maxTagCount"
     key-field="id"
     label-field="name"
     :options="menuTree as any[]"
