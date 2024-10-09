8121355b46d0e9b66677ba547e00e8d4c5d93e25
wansongtao <18101837209@163.com>
2024-10-09 15:56:04 +0800

    feat(components): 更改项目状态组件

---
+++ src/components/button/state/ButtonState.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+import type { AxiosError } from 'axios'
+import type { CSSProperties } from 'vue'
+
+const { id, updateFn } = defineProps<{
+  id: number | string
+  updateFn: (
+    id: number | string,
+    data: { disabled?: boolean }
+  ) => Promise<[err?: AxiosError, data?: any]>
+}>()
+const $emits = defineEmits<{
+  success: []
+}>()
+
+const disabled = defineModel<boolean>()
+
+const loading = ref(false)
+const onChangeState = async (value: boolean) => {
+  const [err] = await updateFn(id, { disabled: value })
+  if (err) {
+    return
+  }
+
+  $emits('success')
+  window.$message.success('操作成功')
+  disabled.value = value
+}
+
+const railStyle = ({ focused, checked }: { focused: boolean; checked: boolean }) => {
+  const style: CSSProperties = {}
+  if (checked) {
+    style.background = '#d03050'
+    if (focused) {
+      style.boxShadow = '0 0 0 2px #d0305040'
+    }
+  }
+
+  return style
+}
+</script>
+
+<template>
+  <n-switch :value="disabled" :loading @update:value="onChangeState" :rail-style="railStyle">
+    <template #checked> 禁用 </template>
+    <template #unchecked> 启用 </template>
+  </n-switch>
+</template>
+
+<style lang="scss" scoped></style>
