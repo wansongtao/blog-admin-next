3ab58dc5a0b1583aaf3af6c5ef1516f8ffb28619
wansongtao <18101837209@163.com>
2024-10-14 10:51:50 +0800

    feat(profile): 个人中心页用户信息组件

--- src/views/profile/ProfileView.vue
+++ src/views/profile/ProfileView.vue
@@ -1,7 +1,31 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import UserCard from './components/UserCard.vue'
+
+import { getProfile } from '@/api/common'
+
+import type { IProfile } from '@/types/api/common'
+
+const profile = ref<IProfile>({
+  userName: '',
+  roles: [],
+  gender: 'OT'
+})
+
+const fetchProfile = async () => {
+  const [, result] = await getProfile()
+  if (result) {
+    profile.value = result.data
+  }
+}
+
+fetchProfile()
+</script>
 
 <template>
-  <div class="profile">profile</div>
+  <n-row :gutter="20">
+    <n-col :span="8"><user-card :profile /></n-col>
+    <n-col :span="16"></n-col>
+  </n-row>
 </template>
 
 <style lang="scss" scoped></style>
