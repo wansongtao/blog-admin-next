1c459dc9c540478c6a6bd24ec5eabae0629ea6f9
wansongtao <18101837209@163.com>
2024-10-15 10:37:17 +0800

    feat(profile): 缓存选中的tab

--- src/views/profile/ProfileView.vue
+++ src/views/profile/ProfileView.vue
@@ -5,6 +5,7 @@ import FormPassword from './components/FormPassword.vue'
 
 import { getProfile } from '@/api/common'
 import { useUserStore } from '@/stores/user'
+import { useRouteQuery } from '@vueuse/router'
 
 import type { IProfile, IProfileParam } from '@/types/api/common'
 
@@ -31,6 +32,8 @@ const onUpdate = (data: IProfileParam) => {
     userStore.userInfo.name = data.nickName ?? profile.value.userName
   }
 }
+
+const tabName = useRouteQuery('profile-tab', 'info')
 </script>
 
 <template>
@@ -38,7 +41,7 @@ const onUpdate = (data: IProfileParam) => {
     <n-gi :span="8"><user-card :profile /></n-gi>
     <n-gi :span="16">
       <n-card style="border-radius: 12px">
-        <n-tabs type="line" animated>
+        <n-tabs v-model:value="tabName" type="line" animated>
           <n-tab-pane name="info" tab="编辑信息">
             <form-user :profile @success="onUpdate" />
           </n-tab-pane>
