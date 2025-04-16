2888580b3c7bf8af92f8c750de598269a06d396b
wansongtao <18101837209@163.com>
2025-04-16 18:40:21 +0800

    feat(api): 添加创建分类接口

--- src/api/category.ts
+++ src/api/category.ts
@@ -1,7 +1,7 @@
 import request from '@/utils/axiosRequest'
 
-import type { IBaseList, IQuery } from '@/types/api'
-import type { ICategoryEntity, ICategoryTreeEntity } from '@/types/api/category'
+import type { IBaseList, IBaseResponse, IQuery } from '@/types/api'
+import type { ICategoryEntity, ICategoryTreeEntity, ICategoryParams } from '@/types/api/category'
 
 export const getCategoryList = (params: IQuery) => {
   return request<IBaseList<ICategoryEntity>>({
@@ -15,5 +15,13 @@ export const getCategoryTree = () => {
   return request<ICategoryTreeEntity[]>({
     url: '/category/tree',
     method: 'get'
+  })
+}
+
+export const addCategory = (data: ICategoryParams) => {
+  return request<IBaseResponse>({
+    url: '/category',
+    method: 'post',
+    data
   })
 }
