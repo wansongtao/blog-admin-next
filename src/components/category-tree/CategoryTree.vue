d8e1a764daa2b1cffbd869d30cb685930acafad2
wansongtao <18101837209@163.com>
2025-04-17 10:48:02 +0800

    feat(components): 分类树选择组件允许禁用某个分类及其所有子类

--- src/components/category-tree/CategoryTree.vue
+++ src/components/category-tree/CategoryTree.vue
@@ -1,19 +1,52 @@
 <script lang="ts" generic="T extends number | null" setup>
 import useCategoryTree from '@/hooks/useCategoryTree'
 
-defineProps<{
+import type { ICategoryTreeEntity } from '@/types/api/category'
+
+const { disabledName } = defineProps<{
   checkable?: boolean
   placeholder?: string
+  disabledName?: string
 }>()
 
 const id = defineModel<T>()
 const { categoryTree } = useCategoryTree(true)
+
+const options = computed(() => {
+  if (disabledName) {
+    const deepDisabled = (list: (ICategoryTreeEntity & { disabled?: boolean })[]) => {
+      list.forEach((v) => {
+        v.disabled = true
+
+        if (v.children?.length) {
+          v.children = deepDisabled(v.children)
+        }
+      })
+
+      return list
+    }
+
+    return categoryTree.value.map((v) => {
+      const disabled = v.name === disabledName
+      if (disabled && v.children?.length) {
+        v.children = deepDisabled(v.children)
+      }
+
+      return {
+        ...v,
+        disabled
+      }
+    })
+  }
+
+  return categoryTree.value
+})
 </script>
 
 <template>
   <n-tree-select
     v-model:value="id"
-    :options="categoryTree"
+    :options
     :placeholder
     :checkable
     key-field="id"
