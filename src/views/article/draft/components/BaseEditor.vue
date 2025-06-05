38d027dede0233da64638135efd51e469599ead9
wansongtao <18101837209@163.com>
2025-06-05 19:02:10 +0800

    feat: markdown 编辑组件

---
+++ src/views/article/draft/components/BaseEditor.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { MdEditor } from 'md-editor-v3'
+import 'md-editor-v3/lib/style.css'
+import { useAppSetStore } from '@/stores/appSet'
+
+defineProps<{
+  previewTheme?: string
+}>()
+
+const appSetStore = useAppSetStore()
+const { theme } = storeToRefs(appSetStore)
+
+const content = defineModel<string>()
+</script>
+
+<template>
+  <MdEditor v-model="content" :theme :toolbars-exclude="['save', 'github']" :preview-theme />
+</template>
+
+<style lang="scss" scoped></style>
